1174c4e6df7599572d226fe5bcfb6d69dc67efb0
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2024-01-16 13:32:51 -0500

    route - post - mercado pago

--- app/api/route.js
+++ app/api/route.js
@@ -1,35 +1,42 @@
 import { MercadoPagoConfig, Preference } from "mercadopago";
 
 export async function POST() {
-  const client = new MercadoPagoConfig({
-    accessToken: process.env.ACCESS_TOKEN,
-  });
+  try {
+    const client = new MercadoPagoConfig({
+      accessToken: process.env.ACCESS_TOKEN,
+    });
 
-  const preference = new Preference(client);
+    const preference = new Preference(client);
 
-  const response = await preference.create({
-    body: {
-      items: [
-        {
-          id: "1",
-          quantity: 5,
-          title: "Fe with me",
-          unit_price: 25,
-          category_id: "photo",
-          description: "friendship",
-          picture_url:
-            "https://res.cloudinary.com/dputhhzyb/image/upload/v1705079802/qzgolsnzdl13wtqwvfk8.jpg",
+    const response = await preference.create({
+      body: {
+        items: [
+          {
+            id: "1",
+            quantity: 5,
+            title: "Fe with me",
+            unit_price: 25,
+            category_id: "photo",
+            description: "friendship",
+            picture_url:
+              "https://res.cloudinary.com/dputhhzyb/image/upload/v1705079802/qzgolsnzdl13wtqwvfk8.jpg",
+          },
+        ],
+        back_urls: {
+          failure: "",
+          pending: "",
+          success: "https://utp-app.vercel.app/",
         },
-      ],
-      back_urls: {
-        failure: "",
-        pending: "",
-        success: "https://utp-app.vercel.app/",
       },
-    },
-  });
+    });
 
-  return Response.json(response, { status: 200 });
+    return Response.json(response, { status: 200 });
+  } catch (error) {
+    return Response.json(error, {
+      status: "400",
+      statusText: "Verify token or other causes",
+    });
+  }
 }
 
 export async function GET() {
