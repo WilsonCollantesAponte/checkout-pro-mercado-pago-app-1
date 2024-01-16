a6a72238df4ae6c65d09145fad7c03f1bd4a8246
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2024-01-16 13:20:12 -0500

    MercadoPago response

--- app/api/route.js
+++ app/api/route.js
@@ -1,5 +1,35 @@
+import { MercadoPagoConfig, Preference } from "mercadopago";
+
 export async function POST() {
-  return Response.json({ data: "ok" }, { status: 200 });
+  const client = new MercadoPagoConfig({
+    accessToken: process.env.ACCESS_TOKEN,
+  });
+
+  const preference = new Preference(client);
+
+  const response = await preference.create({
+    body: {
+      items: [
+        {
+          id: "1",
+          quantity: 5,
+          title: "Fe with me",
+          unit_price: 25,
+          category_id: "photo",
+          description: "friendship",
+          picture_url:
+            "https://res.cloudinary.com/dputhhzyb/image/upload/v1705079802/qzgolsnzdl13wtqwvfk8.jpg",
+        },
+      ],
+      back_urls: {
+        failure: "",
+        pending: "",
+        success: "https://utp-app.vercel.app/",
+      },
+    },
+  });
+
+  return Response.json(response, { status: 200 });
 }
 
 export async function GET() {
